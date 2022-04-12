35dfc86ae200367fac04909db056b47d00963e49
David Colatti <davidcolatti@gmail.com>
2022-04-12 15:12:03 -0400

    set up signin and sign up services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  signup(dto: AuthDto) {
-    console.log({ dto });
+  constructor(
+    private jwt: JwtService,
+    private config: ConfigService,
+    private prisma: PrismaService,
+  ) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const coach = await this.prisma.coach.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      return this.signToken(coach.id, coach.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new HttpException('Credentials Taken', HttpStatus.FORBIDDEN);
+        }
+      }
+      throw error;
+    }
   }
 
-  signin(dto: AuthDto) {
-    console.log({ dto });
+  async signin(dto: AuthDto) {
+    const coach = await this.prisma.coach.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!coach) {
+      throw new HttpException('Invalid Credentials', HttpStatus.FORBIDDEN);
+    }
+
+    const passwordMatches = await argon.verify(coach.hash, dto.password);
+
+    if (!passwordMatches) {
+      throw new HttpException('Invalid Credentials', HttpStatus.FORBIDDEN);
+    }
+
+    return this.signToken(coach.id, coach.email);
+  }
+
+  async signToken(
+    coachId: string,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: coachId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '30d',
+      secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
